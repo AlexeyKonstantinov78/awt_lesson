3e2c90ed2a5bd64c0e5271219db7e7353bc89a51
AlexeyKonstantinov78 <alex_kav@mail.ru>
2025-05-19 13:34:47 +0300

    вывода из программы TextFieldDemo

---
+++ src/main/java/ru/alekseykonstantinov/elements_management_dispatchers_layouts/add_removeelements/TextFieldDemo.java
@@ -0,0 +1,88 @@
+package ru.alekseykonstantinov.elements_management_dispatchers_layouts.add_removeelements;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TextFieldDemo extends Frame implements ActionListener {
+    TextField name, pass;
+
+    /**
+     * Constructs a new instance of {@code Frame} that is
+     * initially invisible.  The title of the {@code Frame}
+     * is empty.
+     *
+     * @throws HeadlessException when
+     *                           {@code GraphicsEnvironment.isHeadless()} returns {@code true}
+     * @see GraphicsEnvironment#isHeadless()
+     * @see Component#setSize
+     * @see Component#setVisible(boolean)
+     */
+    public TextFieldDemo() throws HeadlessException {
+        setLayout(new FlowLayout());
+
+        //элементы
+        Label namep = new Label("Name: ", Label.RIGHT);
+        Label passp = new Label("Passport: ", Label.RIGHT);
+        name = new TextField(12);
+        pass = new TextField(8);
+        pass.setEchoChar('?');
+
+        //добавляем элементы
+        add(namep);
+        add(name);
+        add(passp);
+        add(pass);
+
+        //прослушиватели
+        name.addActionListener(this);
+        pass.addActionListener(this);
+
+
+        addWindowListener(new WindowAdapter() {
+            /**
+             * Invoked when a window is in the process of being closed.
+             * The close operation can be overridden at this point.
+             *
+             * @param e
+             */
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    /**
+     * Invoked when an action occurs.
+     *
+     * @param e the event to be processed
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        repaint();
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param g
+     * @since 1.7
+     */
+    @Override
+    public void paint(Graphics g) {
+        g.drawString("Name: " + name.getText(), 20, 100);
+        g.drawString("Selected text in name: " + name.getSelectedText(), 20, 120);
+        g.drawString("Passport: " + pass.getText(), 20, 140);
+
+    }
+
+    public static void main(String[] args) {
+        TextFieldDemo appwin = new TextFieldDemo();
+        appwin.setSize(new Dimension(380, 200));
+        appwin.setTitle("TextFieldDemo");
+        appwin.setVisible(true);
+    }
+}
